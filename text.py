1d110fc86bacbc5d898453e57d6b3819e2fe3494
Serra KIR DWORCZAK <serrakir@gmail.com>
2024-04-10 10:26:09 +0000

    Add welcome function to text.py

--- text.py
+++ text.py
@@ -1,3 +1,25 @@
 # Imports
 import colorama
 import emoji
+
+
+def welcome():
+    """
+    Welcomes user to the game and gives details about the game.
+    """
+    print("\nWelcome to the Glimpse of the Future \U0001F52E\n")
+    print("With me, you can glance at your future\n")
+    print("and see what's to come!\n")
+    print("\nYou don't need to do much.\n")
+    print("I will do most of the work for you.\n")
+    print("\nAll I need is basic details as: \n")
+    print("your nickname, age, and relationship status.\n")
+    print("\nThere will be four different topics to focus on.\n")
+    print("Health, work, education, and relationship.\n")
+    print("\nYou need to pick one topic to get a chance\n")
+    print("to sneak a peek at your future.\n")
+    print("\nDon't worry if you are hungry for more, \U0001F60C\n")
+    print("You can pick another topic after the first sneak peek.\n")
+    print("\nBut we can get to all that later.\n")
+    print("\nAre you ready? \U0001F9D9\n")
+    print('\nIf so, please type in "yes".\n')
