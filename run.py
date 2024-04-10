1c05a46bd9d0902c4a5a5d6ee85808ff1cdfae74
Serra KIR DWORCZAK <serrakir@gmail.com>
2024-04-10 14:00:42 +0000

    Add start game function to run.py

--- run.py
+++ run.py
@@ -10,3 +10,27 @@ import time
 import colorama
 import emoji
 import text
+
+text.welcome()
+
+
+def game_start():
+    """
+    After the welcome message, asks user if they want to play the game.
+    Checks the user answer if entered in correct method.
+    If answer is yes, moves on to picking a topic to get future predictions.
+    If user answers no, takes user back to welcome message.
+    """
+    text.game_start()
+
+    while True:
+        answer = input(": ")
+        if answer == "yes":
+            """#create function to continue with picking topic"""
+        elif answer == "no":
+            """#create function to ask again if they are sure"""
+        else:
+            """#take back to welcome"""
+
+
+print(game_start())
