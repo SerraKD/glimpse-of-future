08c75eda8bf0f822662b294a6926585cb61bd8c2
Serra KIR DWORCZAK <serrakir@gmail.com>
2024-04-11 15:51:02 +0000

    Add health prediction texts to text.py

--- text.py
+++ text.py
@@ -86,9 +86,77 @@ def topics():
     time.sleep(1)
     print("\nYou need to pick only one topic to get a prediction.\n")
     time.sleep(2)
-    print("1. Health")
-    print("2. Work")
-    print("3. Education")
-    print("3. Relationship")
+    print("1. Health \U0001F3E5")
+    print("2. Work \U0001F4BC")
+    print("3. Education \U0001F4DA")
+    print("3. Relationship \U0001F496")
     time.sleep(2)
     print("\nType the number you selected and press enter.\n")
+
+
+"""
+#Topics
+"""
+
+
+def health_1():
+    """
+    Health future prediction text for age <=25
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nYou are young, wild, and free. \U0001F57A\n")
+    time.sleep(2)
+    print("But you will not stay that way forever!\n")
+    time.sleep(2)
+    print("\nCall me bitter if you will, but I must say,\n")
+    time.sleep(1)
+    print("it would be best if you took better care of yourself.\n")
+    time.sleep(2)
+    print("I don't foresee concerns regarding your health in the future.\n")
+    time.sleep(2)
+    print("\nJust remember my advice and\n")
+    time.sleep(3)
+    print("take care of your physical and mental well-being.\n")
+    time.sleep(1)
+
+
+def health_2():
+    """
+    Health future prediction text for age <=45
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nHmm.. A curious choice for you. \U0001F914\n")
+    time.sleep(2)
+    print("I see that you worry about your health sometimes.\n")
+    time.sleep(1)
+    print("\nWell, you shouldn't.\n")
+    time.sleep(1)
+    print("Instead, do what you need to do to take care of yourself.\n")
+    time.sleep(2)
+    print("I see a few minor health problems that may arise in the future.\n")
+    time.sleep(2)
+    print("But there is no need to panic, everyone has minor issues.\n")
+    time.sleep(3)
+    print("Focus on the good and you will be okay.\n")
+    time.sleep(1)
+
+
+def health_3():
+    """
+    Health future prediction text for age =>46
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nWhat is that sound?!\n")
+    time.sleep(2)
+    print("Oh, it's your bones cracking. \U0001F468\n")
+    time.sleep(1)
+    print("\nI am just kidding of course.\n")
+    time.sleep(1)
+    print("You are in great shape, keep up the good work!\n")
+    time.sleep(2)
+    print("Things may not be easy in the future,\n")
+    time.sleep(2)
+    print("But that's just life, right?\n")
+    time.sleep(3)
+    print("I foresee good health and lots of happiness for you.\n")
+    time.sleep(1)
