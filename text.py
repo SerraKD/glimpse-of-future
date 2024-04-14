a984bbbe7a4af5dfc8b0f28c6943840ce2d3e4dd
Serra KIR DWORCZAK <serrakir@gmail.com>
2024-04-14 19:57:18 +0000

    Create prediction texts for relationship topic in text.py

--- text.py
+++ text.py
@@ -116,21 +116,20 @@ def game_end():
     Displays end game text
     """
     print(Fore.MAGENTA + "\nIt was a pleasure meeting you.\n")
+    time.sleep(1)
     print("\nAnd thank you for letting me take a peek into your future.\n")
     time.sleep(2)
     print("\nBut others need me now, \U0001F9D9\n")
     time.sleep(1)
     print("so I must go.\n")
-    time.sleep(2)
+    time.sleep(1)
     print("We will meet again.\n")
     print("\n \U0001F441 \n")
     time.sleep(3)
     print(Style.RESET_ALL)
 
 
-"""
-#Topics
-"""
+# Prediction texts
 
 
 def health_1():
@@ -311,4 +310,158 @@ def education_3():
     print("study that subject you always wanted.\n")
     time.sleep(2)
     print("You might be surprised how great it will turn out\n")
+    time.sleep(2)
+
+
+def relationship_1():
+    """
+    Relationship future prediction text for age in range(15, 19) and single
+    #https://childmind.org/article/how-to-help-kids-have-good-romantic-relationships/
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nYou should love yourself first, \U0001FAC0\n")
+    time.sleep(2)
+    print("and be your own person.\n")
+    time.sleep(2)
+    print("\nThere will be someone who loves you just the way you are.\n")
+    time.sleep(1)
+    print("That is the kind of person you want to be with.\n")
+    time.sleep(2)
+    print("Your friends are as important as your date,\n")
+    time.sleep(1)
+    print("don't take them for granted.\n")
+    time.sleep(2)
+
+
+def relationship_2():
+    """
+    Relationship future prediction text for age in range(15, 19) and dating
+    #https://childmind.org/article/how-to-help-kids-have-good-romantic-relationships/
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nI see that you and your partner\n")
+    time.sleep(1)
+    print("love each other very much. \U0001F525\n")
+    time.sleep(2)
+    print("\nBeing your own person is an important\n")
+    time.sleep(1)
+    print("part of a happy relationship.\n")
+    time.sleep(2)
+    print("So as not to hide from problems.\n")
+    time.sleep(1)
+    print("I foresee little arguments here and there.\n")
+    time.sleep(2)
+    print("Talk to each other about your feelings and needs.\n")
+    time.sleep(2)
+
+
+def relationship_3():
+    """
+    Relationship future prediction text for age range(19, 45) and single
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nI foresee love is on the corner \U0001F496\n")
+    time.sleep(2)
+    print("You are just not paying attention.\n")
+    time.sleep(2)
+    print("\nThere is a person who is deeply interested in you,\n")
+    time.sleep(1)
+    print("although it may not look like that sometimes.\n")
+    time.sleep(2)
+    print("Give it time and have patience.\n")
+    time.sleep(2)
+
+
+def relationship_4():
+    """
+    Relationship future prediction text for age range(19, 45) and dating
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nI see that you and your partner\n")
+    time.sleep(2)
+    print("love each other very much. \U0001F525\n")
+    time.sleep(2)
+    print("\nDon't let small arguments become a bigger issue.\n")
+    time.sleep(2)
+    print("Learn how to talk to each other effectively and,\n")
+    time.sleep(2)
+    print("try to be understanding of your partner's feelings and needs.\n")
+    time.sleep(2)
+    print("I foresee a great future for both of you.\n")
+    time.sleep(2)
+
+
+def relationship_5():
+    """
+    Relationship future prediction text for age range(19, 45) and married
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nI think you will agree with me, when I say,\n")
+    time.sleep(2)
+    print("marriage is not always easy. \U0001F9D0\n")
+    time.sleep(2)
+    print("\nJust like other good things in life,\n")
+    time.sleep(1)
+    print("you need to put in the work. \n")
+    time.sleep(2)
+    print("Arguments and disagreements are part of being human,\n")
+    time.sleep(1)
+    print("And you will have your fair share of them.\n")
+    time.sleep(2)
+    print("Communicate, pay attention, and understand each other.\n")
+    time.sleep(2)
+
+
+def relationship_6():
+    """
+    Relationship future prediction text for age range(45, 100) and single
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nHmm.. A curious choice for you. \U0001F914\n")
+    time.sleep(2)
+    print("I see that you worry about dating.\n")
+    time.sleep(2)
+    print("\nTake this time in your life to get to know your new self,\n")
+    time.sleep(1)
+    print("do things you always wanted to do, and explore.\n")
+    time.sleep(2)
+    print("Love will knock on your door when you least expect it.\n")
+    time.sleep(2)
+
+
+def relationship_7():
+    """
+    Relationship future prediction text for age range(45, 100) and dating
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nI see you two are great together.\U0001F970\n")
+    time.sleep(2)
+    print("You know that relationships are hard work,\n")
+    time.sleep(2)
+    print("and to get results you have to put in an effort.\n")
+    time.sleep(1)
+    print("I foresee a minor disagreement between you and your partner,\n")
+    time.sleep(2)
+    print("to sort it out try to be understanding of their point of view.\n")
+    time.sleep(2)
+
+
+def relationship_8():
+    """
+    Relationship future prediction text for age range(45, 100) and married
+    """
+    time.sleep(1)
+    print(Fore.GREEN + "\nI think you will agree with me, when I say,\n")
+    time.sleep(1)
+    print("that marriage is not always easy. \U0001F9D0\n")
+    time.sleep(1)
+    print("But you have already overcome many roadblocks.\n")
+    time.sleep(2)
+    print("After some time, people often forget\n")
+    time.sleep(1)
+    print("how important the positive feedback is.\n")
+    time.sleep(2)
+    print("Try to remind your partner that\n")
+    time.sleep(1)
+    print("their companionship is very much appreciated.\n")
     time.sleep(2)
